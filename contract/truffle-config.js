4d56e2db35787c38c01e09dd0c9d3974a81ab37a
Takanori Hirano <hrn.takanori@gmail.com>
2018-07-08 23:43:12 +0900

    :wrench:

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -1,9 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = process.env.MNEMONIC;
+const accessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*',
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          'https://ropsten.infura.io/' + accessToken
+        );
+      },
+      network_id: 3,
+      gas: 2000000,
     },
   },
 };
